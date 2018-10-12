fa3a58a95d94b7a2548ab879efcccf0c93f816b2
Srikaran Sarma <srikarans@riskwizard.com>
2018-10-12 23:49:33 +1100

    HTTP client addd
    HTTP client addd

--- NGLoginCli/src/app/_services/authentication.service.ts
+++ NGLoginCli/src/app/_services/authentication.service.ts
@@ -8,27 +8,31 @@ import { User } from '../_models';
 @Injectable()
 export class AuthenticationService {
   private user: User;
+  private http: HttpClient;
+
+  //constructor(private http: HttpClient) {
+  //}
   
   login(username: string, password: string) {
-    ////return this.http.post<any>(`${config.apiUrl}/users/authenticate`, { username: username, password: password })
-    //return this.http.post<any>(`localhost:4444/users/authenticate`, { username: username, password: password })
-    //  .pipe(map(user => {
-    //    // login successful if there's a jwt token in the response
-    //    if (user && user.token) {
-    //      // store user details and jwt token in local storage to keep user logged in between page refreshes
-    //      localStorage.setItem('currentUser', JSON.stringify(user));
-    //    }
+    //return this.http.post<any>(`${config.apiUrl}/users/authenticate`, { username: username, password: password })
+    return this.http.post<any>(`localhost:4200/users/authenticate`, { username: username, password: password })
+      .pipe(map(user => {
+        // login successful if there's a jwt token in the response
+        if (user && user.token) {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify(user));
+        }
 
-    //    return user;
-    //  }));
+        return user;
+      }));
 
-    this.user = new User();
-    this.user.firstName = 'srikaran';
-    this.user.id = 8855;
-    this.user.lastName = 'n';
-    this.user.username = 'srikarann';
+    
+    //this.user.firstName = 'srikaran';
+    //this.user.id = 8855;
+    //this.user.lastName = 'n';
+    //this.user.username = 'srikarann';
 
-    return this.user;
+    //return this.user;
   }
 
   logout() {
